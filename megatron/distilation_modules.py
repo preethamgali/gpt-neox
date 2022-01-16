3040e6636428c27900e26d8273367c5f724f62d8
preethamgali <preethamgali007@gmail.com>
2022-01-16 12:02:33 +0530

    Distil model func decorator

--- megatron/distilation_modules.py
+++ megatron/distilation_modules.py
@@ -13,22 +13,44 @@ class DistilDecorator:
                 if DistilDecorator.do_distillation:
                     if is_class_function:
                         self, prev_output, input_args, teacher_args, student_args = args
-                        cur_output = class_func(self, prev_output)
+                        # input_args = input_ids, postion_ids, and attention_mask
+                        # teacher_args = hidden_states and output_logits
+                        # student_args = hidden_states and output_logits
+
                         class_name = self.__class__.__name__ 
-                        if class_name in ['EmbeddingPipe', 'SoftEmbedding']:
-                            if input_args[0] is None: # inside students
+
+                        # case when input_ids, position_ids, attention_mask, \
+                        # and teacher_output_logits are provided as input
+                        if class_name in ['EmbeddingPipe', 'SoftEmbedding'] and len(prev_output)==4:
+                            input_args, output_logit = prev_output[:3], prev_output[3]
+                            prev_output = input_args
+                            teacher_args[1] = output_logit
+
+                        # case when input_ids, position_ids, attention_mask, \
+                        # and teacher_hidden_states are provided as input
+                        if class_name in ['ParallelLinearPipe'] and len(prev_output)==4:
+                            input_args, hidden_state = prev_output[:3], prev_output[3]
+                            prev_output = hidden_state
+
+                        cur_output = class_func(self, prev_output)
+
+                        # case when input_ids, position_ids, and attention_mask \
+                        # are provided as input
+                        if class_name in ['EmbeddingPipe', 'SoftEmbedding'] and len(prev_output)==3:
+                            if input_args[0] is None: # inside teacher
                                 input_args = prev_output
                             else:
                                 input_args = (None, None, None)
 
-                        if class_name in ['ParallelLinearPipe']: 
+                        if class_name in ['ParallelLinearPipe'] or \
+                            (class_name in ['EmbeddingPipe'] and len(prev_output) not in [3,4]): 
                             hidden_state = prev_output
-                            output = cur_output
-                            if input_args[0] is None: # inside teacher
-                                student_args = (hidden_state, output)
+                            output_logit = cur_output
+                            if input_args[0] is not None: # inside teacher
+                                teacher_args = (hidden_state, output_logit)
                                 cur_output = input_args
                             else:
-                                teacher_args = (hidden_state, output)
+                                student_args = (hidden_state, output_logit)
 
                     else:
                         non_class_func = class_func
